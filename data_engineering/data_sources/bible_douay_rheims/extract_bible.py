8912164d174b4ba90187a765242e28b89ea84003
Data-Science-Link <mlink@farmersbusinessnetwork.com>
2025-12-25 16:21:40 -0500

    updated ordering of books and adding Bible sections

--- data_engineering/data_sources/bible_douay_rheims/extract_bible.py
+++ data_engineering/data_sources/bible_douay_rheims/extract_bible.py
@@ -68,95 +68,142 @@ OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 # Canonical Catholic Bible order (73 books)
 # This defines the correct order and numbering for all books in the Catholic canon
+# Section labels from USCCB: https://bible.usccb.org/bible
 CATHOLIC_BIBLE_CANON = [
     # Old Testament (1-46)
-    {'id': 'GEN', 'name': 'Genesis', 'canonical_position': 1},
-    {'id': 'EXO', 'name': 'Exodus', 'canonical_position': 2},
-    {'id': 'LEV', 'name': 'Leviticus', 'canonical_position': 3},
-    {'id': 'NUM', 'name': 'Numbers', 'canonical_position': 4},
-    {'id': 'DEU', 'name': 'Deuteronomy', 'canonical_position': 5},
-    {'id': 'JOS', 'name': 'Joshua', 'canonical_position': 6},
-    {'id': 'JDG', 'name': 'Judges', 'canonical_position': 7},
-    {'id': 'RUT', 'name': 'Ruth', 'canonical_position': 8},
-    {'id': '1SA', 'name': '1 Samuel', 'canonical_position': 9},
-    {'id': '2SA', 'name': '2 Samuel', 'canonical_position': 10},
-    {'id': '1KI', 'name': '1 Kings', 'canonical_position': 11},
-    {'id': '2KI', 'name': '2 Kings', 'canonical_position': 12},
-    {'id': '1CH', 'name': '1 Chronicles', 'canonical_position': 13},
-    {'id': '2CH', 'name': '2 Chronicles', 'canonical_position': 14},
-    {'id': 'EZR', 'name': 'Ezra', 'canonical_position': 15},
-    {'id': 'NEH', 'name': 'Nehemiah', 'canonical_position': 16},
-    {'id': 'EST', 'name': 'Esther', 'canonical_position': 17},
-    {'id': 'TOB', 'name': 'Tobit', 'canonical_position': 18},  # Deuterocanonical
-    {'id': 'JDT', 'name': 'Judith', 'canonical_position': 19},  # Deuterocanonical
-    {'id': '1MA', 'name': '1 Maccabees', 'canonical_position': 20},  # Deuterocanonical
-    {'id': '2MA', 'name': '2 Maccabees', 'canonical_position': 21},  # Deuterocanonical
-    {'id': 'JOB', 'name': 'Job', 'canonical_position': 22},
-    {'id': 'PSA', 'name': 'Psalms', 'canonical_position': 23},
-    {'id': 'PRO', 'name': 'Proverbs', 'canonical_position': 24},
-    {'id': 'ECC', 'name': 'Ecclesiastes', 'canonical_position': 25},
-    {'id': 'SNG', 'name': 'Song of Solomon', 'canonical_position': 26},
-    {'id': 'WIS', 'name': 'Wisdom', 'canonical_position': 27},  # Deuterocanonical
-    {'id': 'SIR', 'name': 'Sirach', 'canonical_position': 28},  # Deuterocanonical (Ecclesiasticus)
-    {'id': 'ISA', 'name': 'Isaiah', 'canonical_position': 29},
-    {'id': 'JER', 'name': 'Jeremiah', 'canonical_position': 30},
-    {'id': 'LAM', 'name': 'Lamentations', 'canonical_position': 31},
-    {'id': 'BAR', 'name': 'Baruch', 'canonical_position': 32},  # Deuterocanonical
-    {'id': 'EZK', 'name': 'Ezekiel', 'canonical_position': 33},
-    {'id': 'DAN', 'name': 'Daniel', 'canonical_position': 34},
-    {'id': 'HOS', 'name': 'Hosea', 'canonical_position': 35},
-    {'id': 'JOL', 'name': 'Joel', 'canonical_position': 36},
-    {'id': 'AMO', 'name': 'Amos', 'canonical_position': 37},
-    {'id': 'OBA', 'name': 'Obadiah', 'canonical_position': 38},
-    {'id': 'JON', 'name': 'Jonah', 'canonical_position': 39},
-    {'id': 'MIC', 'name': 'Micah', 'canonical_position': 40},
-    {'id': 'NAM', 'name': 'Nahum', 'canonical_position': 41},
-    {'id': 'HAB', 'name': 'Habakkuk', 'canonical_position': 42},
-    {'id': 'ZEP', 'name': 'Zephaniah', 'canonical_position': 43},
-    {'id': 'HAG', 'name': 'Haggai', 'canonical_position': 44},
-    {'id': 'ZEC', 'name': 'Zechariah', 'canonical_position': 45},
-    {'id': 'MAL', 'name': 'Malachi', 'canonical_position': 46},
+    # The Pentateuch
+    {'id': 'GEN', 'name': 'Genesis', 'canonical_position': 1, 'section': 'The Pentateuch'},
+    {'id': 'EXO', 'name': 'Exodus', 'canonical_position': 2, 'section': 'The Pentateuch'},
+    {'id': 'LEV', 'name': 'Leviticus', 'canonical_position': 3, 'section': 'The Pentateuch'},
+    {'id': 'NUM', 'name': 'Numbers', 'canonical_position': 4, 'section': 'The Pentateuch'},
+    {'id': 'DEU', 'name': 'Deuteronomy', 'canonical_position': 5, 'section': 'The Pentateuch'},
+    # Historical Introduction
+    {'id': 'JOS', 'name': 'Joshua', 'canonical_position': 6, 'section': 'Historical Introduction'},
+    {'id': 'JDG', 'name': 'Judges', 'canonical_position': 7, 'section': 'Historical Introduction'},
+    {'id': 'RUT', 'name': 'Ruth', 'canonical_position': 8, 'section': 'Historical Introduction'},
+    {'id': '1SA', 'name': '1 Samuel', 'canonical_position': 9, 'section': 'Historical Introduction'},
+    {'id': '2SA', 'name': '2 Samuel', 'canonical_position': 10, 'section': 'Historical Introduction'},
+    {'id': '1KI', 'name': '1 Kings', 'canonical_position': 11, 'section': 'Historical Introduction'},
+    {'id': '2KI', 'name': '2 Kings', 'canonical_position': 12, 'section': 'Historical Introduction'},
+    {'id': '1CH', 'name': '1 Chronicles', 'canonical_position': 13, 'section': 'Historical Introduction'},
+    {'id': '2CH', 'name': '2 Chronicles', 'canonical_position': 14, 'section': 'Historical Introduction'},
+    {'id': 'EZR', 'name': 'Ezra', 'canonical_position': 15, 'section': 'Historical Introduction'},
+    {'id': 'NEH', 'name': 'Nehemiah', 'canonical_position': 16, 'section': 'Historical Introduction'},
+    # Biblical Novellas
+    {'id': 'TOB', 'name': 'Tobit', 'canonical_position': 17, 'section': 'Biblical Novellas'},  # Deuterocanonical
+    {'id': 'JDT', 'name': 'Judith', 'canonical_position': 18, 'section': 'Biblical Novellas'},  # Deuterocanonical
+    {'id': 'EST', 'name': 'Esther', 'canonical_position': 19, 'section': 'Biblical Novellas'},
+    {'id': '1MA', 'name': '1 Maccabees', 'canonical_position': 20, 'section': 'Biblical Novellas'},  # Deuterocanonical
+    {'id': '2MA', 'name': '2 Maccabees', 'canonical_position': 21, 'section': 'Biblical Novellas'},  # Deuterocanonical
+    # Wisdom Books
+    {'id': 'JOB', 'name': 'Job', 'canonical_position': 22, 'section': 'Wisdom Books'},
+    {'id': 'PSA', 'name': 'Psalms', 'canonical_position': 23, 'section': 'Wisdom Books'},
+    {'id': 'PRO', 'name': 'Proverbs', 'canonical_position': 24, 'section': 'Wisdom Books'},
+    {'id': 'ECC', 'name': 'Ecclesiastes', 'canonical_position': 25, 'section': 'Wisdom Books'},
+    {'id': 'SNG', 'name': 'Song of Solomon', 'canonical_position': 26, 'section': 'Wisdom Books'},
+    {'id': 'WIS', 'name': 'Wisdom', 'canonical_position': 27, 'section': 'Wisdom Books'},  # Deuterocanonical
+    {'id': 'SIR', 'name': 'Sirach', 'canonical_position': 28, 'section': 'Wisdom Books'},  # Deuterocanonical (Ecclesiasticus)
+    # Prophetic Books
+    {'id': 'ISA', 'name': 'Isaiah', 'canonical_position': 29, 'section': 'Prophetic Books'},
+    {'id': 'JER', 'name': 'Jeremiah', 'canonical_position': 30, 'section': 'Prophetic Books'},
+    {'id': 'LAM', 'name': 'Lamentations', 'canonical_position': 31, 'section': 'Prophetic Books'},
+    {'id': 'BAR', 'name': 'Baruch', 'canonical_position': 32, 'section': 'Prophetic Books'},  # Deuterocanonical
+    {'id': 'EZK', 'name': 'Ezekiel', 'canonical_position': 33, 'section': 'Prophetic Books'},
+    {'id': 'DAN', 'name': 'Daniel', 'canonical_position': 34, 'section': 'Prophetic Books'},
+    {'id': 'HOS', 'name': 'Hosea', 'canonical_position': 35, 'section': 'Prophetic Books'},
+    {'id': 'JOL', 'name': 'Joel', 'canonical_position': 36, 'section': 'Prophetic Books'},
+    {'id': 'AMO', 'name': 'Amos', 'canonical_position': 37, 'section': 'Prophetic Books'},
+    {'id': 'OBA', 'name': 'Obadiah', 'canonical_position': 38, 'section': 'Prophetic Books'},
+    {'id': 'JON', 'name': 'Jonah', 'canonical_position': 39, 'section': 'Prophetic Books'},
+    {'id': 'MIC', 'name': 'Micah', 'canonical_position': 40, 'section': 'Prophetic Books'},
+    {'id': 'NAM', 'name': 'Nahum', 'canonical_position': 41, 'section': 'Prophetic Books'},
+    {'id': 'HAB', 'name': 'Habakkuk', 'canonical_position': 42, 'section': 'Prophetic Books'},
+    {'id': 'ZEP', 'name': 'Zephaniah', 'canonical_position': 43, 'section': 'Prophetic Books'},
+    {'id': 'HAG', 'name': 'Haggai', 'canonical_position': 44, 'section': 'Prophetic Books'},
+    {'id': 'ZEC', 'name': 'Zechariah', 'canonical_position': 45, 'section': 'Prophetic Books'},
+    {'id': 'MAL', 'name': 'Malachi', 'canonical_position': 46, 'section': 'Prophetic Books'},
     # New Testament (47-73)
-    {'id': 'MAT', 'name': 'Matthew', 'canonical_position': 47},
-    {'id': 'MRK', 'name': 'Mark', 'canonical_position': 48},
-    {'id': 'LUK', 'name': 'Luke', 'canonical_position': 49},
-    {'id': 'JHN', 'name': 'John', 'canonical_position': 50},
-    {'id': 'ACT', 'name': 'Acts', 'canonical_position': 51},
-    {'id': 'ROM', 'name': 'Romans', 'canonical_position': 52},
-    {'id': '1CO', 'name': '1 Corinthians', 'canonical_position': 53},
-    {'id': '2CO', 'name': '2 Corinthians', 'canonical_position': 54},
-    {'id': 'GAL', 'name': 'Galatians', 'canonical_position': 55},
-    {'id': 'EPH', 'name': 'Ephesians', 'canonical_position': 56},
-    {'id': 'PHP', 'name': 'Philippians', 'canonical_position': 57},
-    {'id': 'COL', 'name': 'Colossians', 'canonical_position': 58},
-    {'id': '1TH', 'name': '1 Thessalonians', 'canonical_position': 59},
-    {'id': '2TH', 'name': '2 Thessalonians', 'canonical_position': 60},
-    {'id': '1TI', 'name': '1 Timothy', 'canonical_position': 61},
-    {'id': '2TI', 'name': '2 Timothy', 'canonical_position': 62},
-    {'id': 'TIT', 'name': 'Titus', 'canonical_position': 63},
-    {'id': 'PHM', 'name': 'Philemon', 'canonical_position': 64},
-    {'id': 'HEB', 'name': 'Hebrews', 'canonical_position': 65},
-    {'id': 'JAS', 'name': 'James', 'canonical_position': 66},
-    {'id': '1PE', 'name': '1 Peter', 'canonical_position': 67},
-    {'id': '2PE', 'name': '2 Peter', 'canonical_position': 68},
-    {'id': '1JN', 'name': '1 John', 'canonical_position': 69},
-    {'id': '2JN', 'name': '2 John', 'canonical_position': 70},
-    {'id': '3JN', 'name': '3 John', 'canonical_position': 71},
-    {'id': 'JUD', 'name': 'Jude', 'canonical_position': 72},
-    {'id': 'REV', 'name': 'Revelation', 'canonical_position': 73},
+    # The Gospels
+    {'id': 'MAT', 'name': 'Matthew', 'canonical_position': 47, 'section': 'The Gospels'},
+    {'id': 'MRK', 'name': 'Mark', 'canonical_position': 48, 'section': 'The Gospels'},
+    {'id': 'LUK', 'name': 'Luke', 'canonical_position': 49, 'section': 'The Gospels'},
+    {'id': 'JHN', 'name': 'John', 'canonical_position': 50, 'section': 'The Gospels'},
+    {'id': 'ACT', 'name': 'Acts', 'canonical_position': 51, 'section': 'Acts of the Apostles'},
+    # New Testament Letters
+    {'id': 'ROM', 'name': 'Romans', 'canonical_position': 52, 'section': 'New Testament Letters'},
+    {'id': '1CO', 'name': '1 Corinthians', 'canonical_position': 53, 'section': 'New Testament Letters'},
+    {'id': '2CO', 'name': '2 Corinthians', 'canonical_position': 54, 'section': 'New Testament Letters'},
+    {'id': 'GAL', 'name': 'Galatians', 'canonical_position': 55, 'section': 'New Testament Letters'},
+    {'id': 'EPH', 'name': 'Ephesians', 'canonical_position': 56, 'section': 'New Testament Letters'},
+    {'id': 'PHP', 'name': 'Philippians', 'canonical_position': 57, 'section': 'New Testament Letters'},
+    {'id': 'COL', 'name': 'Colossians', 'canonical_position': 58, 'section': 'New Testament Letters'},
+    {'id': '1TH', 'name': '1 Thessalonians', 'canonical_position': 59, 'section': 'New Testament Letters'},
+    {'id': '2TH', 'name': '2 Thessalonians', 'canonical_position': 60, 'section': 'New Testament Letters'},
+    {'id': '1TI', 'name': '1 Timothy', 'canonical_position': 61, 'section': 'New Testament Letters'},
+    {'id': '2TI', 'name': '2 Timothy', 'canonical_position': 62, 'section': 'New Testament Letters'},
+    {'id': 'TIT', 'name': 'Titus', 'canonical_position': 63, 'section': 'New Testament Letters'},
+    {'id': 'PHM', 'name': 'Philemon', 'canonical_position': 64, 'section': 'New Testament Letters'},
+    {'id': 'HEB', 'name': 'Hebrews', 'canonical_position': 65, 'section': 'New Testament Letters'},
+    # Catholic Letters
+    {'id': 'JAS', 'name': 'James', 'canonical_position': 66, 'section': 'Catholic Letters'},
+    {'id': '1PE', 'name': '1 Peter', 'canonical_position': 67, 'section': 'Catholic Letters'},
+    {'id': '2PE', 'name': '2 Peter', 'canonical_position': 68, 'section': 'Catholic Letters'},
+    {'id': '1JN', 'name': '1 John', 'canonical_position': 69, 'section': 'Catholic Letters'},
+    {'id': '2JN', 'name': '2 John', 'canonical_position': 70, 'section': 'Catholic Letters'},
+    {'id': '3JN', 'name': '3 John', 'canonical_position': 71, 'section': 'Catholic Letters'},
+    {'id': 'JUD', 'name': 'Jude', 'canonical_position': 72, 'section': 'Catholic Letters'},
+    {'id': 'REV', 'name': 'Revelation', 'canonical_position': 73, 'section': 'Revelation'},
 ]
 
 # Deuterocanonical books missing from bible-api.com
 # These are the 7 books that should be in the Catholic canon but are not returned by the API
+# Order matches USCCB canonical order: https://bible.usccb.org/bible
 DEUTEROCANONICAL_BOOKS = [
-    {'id': 'TOB', 'name': 'Tobit', 'canonical_position': 18},
-    {'id': 'JDT', 'name': 'Judith', 'canonical_position': 19},
-    {'id': 'WIS', 'name': 'Wisdom', 'canonical_position': 27},
-    {'id': 'SIR', 'name': 'Sirach', 'canonical_position': 28},
-    {'id': 'BAR', 'name': 'Baruch', 'canonical_position': 32},
-    {'id': '1MA', 'name': '1 Maccabees', 'canonical_position': 20},
-    {'id': '2MA', 'name': '2 Maccabees', 'canonical_position': 21},
+    {'id': 'TOB', 'name': 'Tobit', 'canonical_position': 17},  # Biblical Novellas
+    {'id': 'JDT', 'name': 'Judith', 'canonical_position': 18},  # Biblical Novellas
+    {'id': 'WIS', 'name': 'Wisdom', 'canonical_position': 27},  # Wisdom Books
+    {'id': 'SIR', 'name': 'Sirach', 'canonical_position': 28},  # Wisdom Books
+    {'id': 'BAR', 'name': 'Baruch', 'canonical_position': 32},  # Prophetic Books
+    {'id': '1MA', 'name': '1 Maccabees', 'canonical_position': 20},  # Biblical Novellas
+    {'id': '2MA', 'name': '2 Maccabees', 'canonical_position': 21},  # Biblical Novellas
 ]
+
+
+def get_canonical_info(book_id: str, book_name: str = None) -> Optional[Dict[str, Any]]:
+    """Get the canonical information for a book in the Catholic Bible.
+
+    Args:
+        book_id: The book identifier (e.g., 'GEN', 'EXO')
+        book_name: Optional book name for fallback matching
+
+    Returns:
+        Dictionary with 'canonical_position' and 'section', or None if not found
+    """
+    # First try to match by ID
+    for book in CATHOLIC_BIBLE_CANON:
+        if book['id'] == book_id:
+            return {
+                'canonical_position': book['canonical_position'],
+                'section': book.get('section', '')
+            }
+
+    # Fallback: try to match by name (case-insensitive, handle variations)
+    if book_name:
+        book_name_lower = book_name.lower().strip()
+        for book in CATHOLIC_BIBLE_CANON:
+            if book['name'].lower() == book_name_lower:
+                return {
+                    'canonical_position': book['canonical_position'],
+                    'section': book.get('section', '')
+                }
+            # Handle variations like "1 Samuel" vs "1 Samuel"
+            if book_name_lower.replace(' ', '') == book['name'].lower().replace(' ', ''):
+                return {
+                    'canonical_position': book['canonical_position'],
+                    'section': book.get('section', '')
+                }
+
+    return None
 
 
 def get_canonical_position(book_id: str, book_name: str = None) -> Optional[int]:
@@ -169,22 +216,8 @@ def get_canonical_position(book_id: str, book_name: str = None) -> Optional[int]
     Returns:
         Canonical position (1-73), or None if not found
     """
-    # First try to match by ID
-    for book in CATHOLIC_BIBLE_CANON:
-        if book['id'] == book_id:
-            return book['canonical_position']
-
-    # Fallback: try to match by name (case-insensitive, handle variations)
-    if book_name:
-        book_name_lower = book_name.lower().strip()
-        for book in CATHOLIC_BIBLE_CANON:
-            if book['name'].lower() == book_name_lower:
-                return book['canonical_position']
-            # Handle variations like "1 Samuel" vs "1 Samuel"
-            if book_name_lower.replace(' ', '') == book['name'].lower().replace(' ', ''):
-                return book['canonical_position']
-
-    return None
+    info = get_canonical_info(book_id, book_name)
+    return info['canonical_position'] if info else None
 
 
 def get_deuterocanonical_books() -> List[Dict[str, Any]]:
@@ -335,12 +368,21 @@ def generate_markdown(book_name: str, book_id: str, canonical_position: Optional
         logger.warning(f"Invalid book data for {book_name}")
         return False
 
-    # Get canonical position if not provided
+    # Get canonical info if not provided
+    canonical_info = None
     if canonical_position is None:
-        canonical_position = get_canonical_position(book_id, book_name)
-        if canonical_position is None:
+        canonical_info = get_canonical_info(book_id, book_name)
+        if canonical_info:
+            canonical_position = canonical_info['canonical_position']
+        else:
             logger.warning(f"Could not determine canonical position for {book_name} ({book_id}), using fallback")
             canonical_position = 99  # Fallback for unknown books
+    else:
+        # Get section info even if position is provided
+        canonical_info = get_canonical_info(book_id, book_name)
+
+    # Get section label from canonical info
+    section = canonical_info.get('section', '') if canonical_info else ''
 
     # Determine testament (Old Testament: 1-46, New Testament: 47-73)
     testament = "Old Testament" if canonical_position <= 46 else "New Testament"
@@ -364,6 +406,8 @@ def generate_markdown(book_name: str, book_id: str, canonical_position: Optional
             f.write(f"title: {book_name}\n")
             f.write(f"canonical_position: {canonical_position}\n")
             f.write(f"testament: {testament}\n")
+            if section:
+                f.write(f"section: {section}\n")
             f.write(f"book_id: {book_id}\n")
             f.write(f"translation: Douay-Rheims 1899 American Edition\n")
             f.write(f"total_chapters: {total_chapters_in_book}\n")
@@ -373,6 +417,10 @@ def generate_markdown(book_name: str, book_id: str, canonical_position: Optional
             f.write(f"  - bible\n")
             f.write(f"  - douay-rheims\n")
             f.write(f"  - {testament.lower().replace(' ', '-')}\n")
+            if section:
+                # Add section as a tag (lowercase, hyphenated)
+                section_tag = section.lower().replace(' ', '-')
+                f.write(f"  - {section_tag}\n")
             f.write(f"  - catholic-canon\n")
             f.write(f"language: en\n")
             f.write(f"format: markdown\n")
